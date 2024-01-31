0aefd9662a702083233011266e0d9c6c850d3dd3
leoniefaerber <faerber.leonie@gmx.de>
2024-01-31 13:22:33 +0100

    Fix exercise 5

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -18,22 +18,19 @@ def validate_coordinates(lat, lon):
     return -90 <= lat <= 90 and -90 <= lon <= 90
 
 def filter_and_validate_stops(stops_df):
-    filtered_stops = stops_df[stops_df['zone_id'] == 2001]
+    filtered_stops = stops_df[['stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'zone_id']]
+    filtered_stops = filtered_stops[stops_df['zone_id'] == 2001]
     filtered_stops = filtered_stops[filtered_stops.apply(lambda row: validate_coordinates(row['stop_lat'], row['stop_lon']), axis=1)]
-    filtered_stops = filtered_stops.dropna(subset=['stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'zone_id'])
+    filtered_stops = filtered_stops.dropna()
     return filtered_stops
 
 def create_sqlite_types():
-    return {'stop_id': 'TEXT', 'stop_name': 'TEXT', 'stop_lat': 'FLOAT', 'stop_lon': 'FLOAT', 'zone_id': 'BIGINT'}
+    return {'stop_id': 'INTEGER', 'stop_name': 'TEXT', 'stop_lat': 'FLOAT', 'stop_lon': 'FLOAT', 'zone_id': 'INTEGER'}
 
-def create_sqlite_connection(db_name):
-    return sqlite3.connect(db_name)
-
-def write_to_sqlite(data_frame, conn, table_name, sqlite_types):
-    data_frame.to_sql(table_name, conn, index=False, if_exists='replace', dtype=sqlite_types)
-
-def cleanup(zip_file_path):
-    os.remove(zip_file_path)
+def write_to_sqlite(data_frame, table_name, file_name):
+    conn = sqlite3.connect(file_name)
+    data_frame.to_sql(table_name, conn, index=False, if_exists='replace', dtype=create_sqlite_types())
+    conn.close()
 
 def main():
     # download gtfs data
@@ -46,13 +43,10 @@ def main():
     filtered_stops = filter_and_validate_stops(stops_df)
 
     # write data to sqlite
-    sqlite_types = create_sqlite_types()
-    conn = create_sqlite_connection('gtfs.sqlite')
-    write_to_sqlite(filtered_stops, conn, 'stops', sqlite_types)
+    write_to_sqlite(filtered_stops, 'stops', 'gtfs.sqlite')
 
-    # close connection and cleanup
-    conn.close()
-    cleanup("GTFS.zip")
+    # cleanup
+    os.remove('GTFS.zip')
 
 if __name__ == "__main__":
     main()
