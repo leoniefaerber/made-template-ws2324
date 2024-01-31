ebda7bbe1b3d38e145c4b8b666d7840a1751086c
leoniefaerber <faerber.leonie@gmx.de>
2024-01-31 14:19:50 +0100

    Improve ex 5

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -2,17 +2,14 @@ import zipfile
 import os
 import urllib.request
 import pandas as pd
-import sqlite3
+from sqlalchemy import TEXT, FLOAT, INTEGER
+import sqlalchemy as sa
 
 def download_gtfs_data(url, zip_file_path):
     urllib.request.urlretrieve(url, zip_file_path)
 
 def read_stops_txt(zip_file_path):
-    stops_data = None
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        with zip_ref.open('stops.txt') as stops_file:
-            stops_data = pd.read_csv(stops_file)
-    return stops_data
+    return pd.read_csv(zipfile.ZipFile(zip_file_path).open('stops.txt'))
 
 def validate_coordinates(lat, lon):
     return -90 <= lat <= 90 and -90 <= lon <= 90
@@ -25,12 +22,11 @@ def filter_and_validate_stops(stops_df):
     return filtered_stops
 
 def create_sqlite_types():
-    return {'stop_id': 'INTEGER', 'stop_name': 'TEXT', 'stop_lat': 'FLOAT', 'stop_lon': 'FLOAT', 'zone_id': 'INTEGER'}
+    return {'stop_id': INTEGER, 'stop_name': TEXT, 'stop_lat': FLOAT, 'stop_lon': FLOAT, 'zone_id': INTEGER}
 
-def write_to_sqlite(data_frame, table_name, file_name):
-    conn = sqlite3.connect(file_name)
+def write_to_sqlite(data_frame, table_name):
+    conn = sa.create_engine("sqlite:///gtfs.sqlite")
     data_frame.to_sql(table_name, conn, index=False, if_exists='replace', dtype=create_sqlite_types())
-    conn.close()
 
 def main():
     # download gtfs data
@@ -43,7 +39,7 @@ def main():
     filtered_stops = filter_and_validate_stops(stops_df)
 
     # write data to sqlite
-    write_to_sqlite(filtered_stops, 'stops', 'gtfs.sqlite')
+    write_to_sqlite(filtered_stops, 'stops')
 
     # cleanup
     os.remove('GTFS.zip')
